8dcd7e667b8019b51e88c436ca753d13e79a99e1
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-02-03 11:17:11 +0100

    JECOMMITLEPROJETSURGITHUB

--- API/Program.cs
+++ API/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<SeriesDbContext>(options =>
-  options.UseNpgsql(builder.Configuration.GetConnectionString("SeriesDbContext")));
+  options.UseNpgsql(builder.Configuration.GetConnectionString("SeriesDbContextRemote")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
