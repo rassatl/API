9b01d308b76dbf5b5eb8623b50995214e80c018f
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-02-03 11:00:52 +0100

    2eme caca ajoute

--- APITests1/Controllers/SeriesControllerTests.cs
+++ APITests1/Controllers/SeriesControllerTests.cs
@@ -36,12 +36,7 @@ namespace API.Controllers.Tests
         [TestMethod()]
         public void GetSeriesTest()
         {
-            Assert.Fail();
-        }
 
-        [TestMethod()]
-        public void GetSerieTest()
-        {
             List<Serie> serieList = new List<Serie>();
             serieList.AddRange(new List<Serie> {
                 new Serie(
@@ -76,7 +71,25 @@ namespace API.Controllers.Tests
             List<Serie> listeSeriesRecuperees = new List<Serie>();
             listeSeriesRecuperees = getSeries.Value.Where(s => s.Serieid <= 3).ToList();
 
-            CollectionAssert.AreEqual(serieList, listeSeriesRecuperees, "errrrrrrrrrrrrrr");
+            CollectionAssert.AreEqual(serieList, listeSeriesRecuperees);
+        }
+
+        [TestMethod()]
+        public void GetSerieTest()
+        {
+            Serie serieList = new Serie(
+                    1,
+                    "Scrubs",
+                    "J.D. est un jeune médecin qui débute sa carrière dans l'hôpital du Sacré-Coeur. Il vit avec son meilleur ami Turk, qui lui est chirurgien dans le même hôpital. Très vite, Turk tombe amoureux d'une infirmière Carla. Elliot entre dans la bande. C'est une étudiante en médecine quelque peu surprenante. Le service de médecine est dirigé par l'excentrique Docteur Cox alors que l'hôpital est géré par le diabolique Docteur Kelso. A cela viennent s'ajouter plein de personnages hors du commun : Todd le chirurgien obsédé, Ted l'avocat dépressif, le concierge qui trouve toujours un moyen d'embêter JD... Une belle galerie de personnage !",
+                    9,
+                    184,
+                    2001,
+                    "ABC (US)");
+
+            var getSeries = SeriesController.GetSerie(1).Result.Value;
+            Assert.AreEqual(serieList, getSeries);
+
+            //CollectionAssert.AreEqual(serieList, listeSeriesRecuperees);
         }
 
         [TestMethod()]
