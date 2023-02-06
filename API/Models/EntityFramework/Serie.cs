27d9deb30261fa8f69d0cb555b726d9873d1f9db
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-02-06 08:58:54 +0100

    TP1FINI

--- API/Models/EntityFramework/Serie.cs
+++ API/Models/EntityFramework/Serie.cs
@@ -43,21 +43,4 @@ public partial class Serie
     [Column("network")]
     [StringLength(50)]
     public string? Network { get; set; }
-
-    public override bool Equals(object? obj)
-    {
-        return obj is Serie serie &&
-               Serieid == serie.Serieid &&
-               Titre == serie.Titre &&
-               Resume == serie.Resume &&
-               Nbsaisons == serie.Nbsaisons &&
-               Nbepisodes == serie.Nbepisodes &&
-               Anneecreation == serie.Anneecreation &&
-               Network == serie.Network;
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(Serieid, Titre, Resume, Nbsaisons, Nbepisodes, Anneecreation, Network);
-    }
 }
