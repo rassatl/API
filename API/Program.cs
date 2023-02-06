3dbe18bdea01bb0107aa88c9c68ae4b05b0f9d98
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-02-06 11:06:44 +0100

    test api

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<SeriesDbContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
